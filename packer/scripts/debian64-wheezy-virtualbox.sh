8b4a1ba206fb5e3b1592a4bdbdfc13bf1359ea9e
Darrel O'Pry <darrel.opry@spry-group.com>
2014-03-24 21:10:32 -0400

    Remove obsolete veeewee share validation mount.

--- packer/scripts/debian64-wheezy-virtualbox.sh
+++ packer/scripts/debian64-wheezy-virtualbox.sh
@@ -25,10 +25,4 @@ if test -f .vbox_version ; then
 
   # Start the newly build driver
   /etc/init.d/vboxadd start
-
-  # Make a temporary mount point
-  mkdir /tmp/veewee-validation
-
-  # Test mount the veewee-validation
-  mount -t vboxsf veewee-validation /tmp/veewee-validation
 fi
