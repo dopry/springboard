0c1752299485652546be0a5c557c16055adb523c
Darrel O'Pry <darrel.opry@spry-group.com>
2014-03-24 22:02:23 -0400

    Add known_host and pub/priv keys to enable ssh loopback with ansible.

--- packer/scripts/debian64-wheezy-vagrant.sh
+++ packer/scripts/debian64-wheezy-vagrant.sh
@@ -9,7 +9,15 @@ adduser adm vagrant
 # Install vagrant keys
 mkdir -pm 700 /home/vagrant/.ssh
 curl -Lo /home/vagrant/.ssh/authorized_keys 'https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub'
+curl -Lo /home/vagrant/.ssh/id_rsa 'https://raw.github.com/mitchellh/vagrant/master/keys/vagrant'
+curl -Lo /home/vagrant/.ssh/id_rsa.pub 'https://raw.github.com/mitchellh/vagrant/master/keys/vagrant'
+ssh-keyscan -t rsa,dsa 127.0.0.1  2>&1 > /home/vagrant/.ssh/known_hosts
+
 chmod 0600 /home/vagrant/.ssh/authorized_keys
+chmod 0600 /home/vagrant/.ssh/id_rsa
+chmod 0644 /home/vagrant/.ssh/id_rsa.pub
+chmod 0644 /home/vagrant/.ssh/known_hosts
+
 chown -R vagrant:vagrant /home/vagrant/.ssh
 
 # Customize the message of the day
