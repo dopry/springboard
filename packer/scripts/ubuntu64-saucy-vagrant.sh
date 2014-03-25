d851e2bf1817b1e07a96de1784c1afaf329faa33
Darrel O'Pry <darrel.opry@spry-group.com>
2014-03-24 22:19:40 -0400

    remove deprecated lines.

--- packer/scripts/ubuntu64-saucy-vagrant.sh
+++ packer/scripts/ubuntu64-saucy-vagrant.sh
@@ -1,11 +1,3 @@
-mkdir /home/vagrant/.ssh
-chmod 700 /home/vagrant/.ssh
-cd /home/vagrant/.ssh
-wget --no-check-certificate 'https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub' -O authorized_keys
-chmod 600 /home/vagrant/.ssh/authorized_keys
-chown -R vagrant /home/vagrant/.ssh
-
-
 # Set up Vagrant.
 date > /etc/vagrant_box_build_time
 
