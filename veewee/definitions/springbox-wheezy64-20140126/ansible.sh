18d7af405668c2ed6e42a06ba58b5e147b93969b
Darrel O'Pry <darrel.opry@spry-group.com>
2014-03-24 20:22:19 -0400

    remove obsolete veewee components.

--- veewee/definitions/springbox-wheezy64-20140126/ansible.sh
+++
@@ -1,4 +0,0 @@
- wget http://ansibleworks.com/releases/ansible-1.4.4.tar.gz -O /opt/ansible-1.4.4.tar.gz
- tar -C /opt -xzf /opt/ansible-1.4.4.tar.gz
- ln -s /opt/ansible-1.4.4 /opt/ansible
- echo PATH="$PATH:/opt/ansible/bin" >> /etc/profile.d/ansible.sh
