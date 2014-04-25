965b922466a17eca19049250d59bf6a1e6f42809
Darrel O'Pry <darrel.opry@spry-group.com>
2014-04-25 11:22:23 -0400

    Add ansible keys to root.

--- packer/scripts/debian64-wheezy-vagrant.sh
+++ packer/scripts/debian64-wheezy-vagrant.sh
@@ -20,5 +20,18 @@ chmod 0644 /home/vagrant/.ssh/known_hosts
 
 chown -R vagrant:vagrant /home/vagrant/.ssh
 
+# Install vagrant keys
+mkdir -pm 700 /root/.ssh
+curl -Lo /root/.ssh/authorized_keys 'https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub'
+curl -Lo /root/.ssh/id_rsa 'https://raw.github.com/mitchellh/vagrant/master/keys/vagrant'
+curl -Lo /root/.ssh/id_rsa.pub 'https://raw.github.com/mitchellh/vagrant/master/keys/vagrant'
+ssh-keyscan -t rsa,dsa 127.0.0.1  2>&1 > /root/.ssh/known_hosts
+
+chmod 0600 /root/.ssh/authorized_keys
+chmod 0600 /root/.ssh/id_rsa
+chmod 0644 /root/.ssh/id_rsa.pub
+chmod 0644 /root/.ssh/known_hosts
+chown -R root:root /root/.ssh
+
 # Customize the message of the day
 echo 'Springboard! Jump in!' > /var/run/motd
